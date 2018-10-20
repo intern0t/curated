53135df4ac82f511ca5dbc6396a939f2dff9b2b4
Prashant Shrestha <intern0t@users.noreply.github.com>
2018-10-20 17:30:48 -0400

    More features related to Bookmark.

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-const Bookmarks = ({ bookmarked }) => {
+const Bookmarks = ({ bookmarked, viewTypeIsNews, onViewChange }) => {
     return (
         <div className="bookmark-section">
             You have <span className="count">{bookmarked.length}</span>{" "}
             bookmarks. Press the <span className="icon-bookmark-outline-add" />{" "}
-            button to add to your bookmarked list. View bookmarks.
+            button to add to your bookmarked list.{" "}
+            <span className="view-bookmarks" onClick={onViewChange}>
+                {`View ${viewTypeIsNews ? "Bookmarks" : "News"}`}
+            </span>
         </div>
     );
 };
--- src/components/NewsWrapper.js
+++ src/components/NewsWrapper.js
@@ -5,8 +5,8 @@ import uuid from "uuid/v4";
 const NewsWrapper = ({ news, bookmarked, onBookmarkToggle }) => {
     return (
         <div className="news-container">
-            {news.news && news.news.length > 0 ? (
-                news.news.map(_news => {
+            {news && news.length > 0 ? (
+                news.map(_news => {
                     return (
                         <News
                             news={_news}
@@ -17,11 +17,7 @@ const NewsWrapper = ({ news, bookmarked, onBookmarkToggle }) => {
                     );
                 })
             ) : (
-                <div className="warn">
-                    {
-                        "There seems to be no news! Are you absolutely sure that you set your NewsAPI.org's API key?"
-                    }
-                </div>
+                <div className="warn">{"There seems to be no news!"}</div>
             )}
         </div>
     );
